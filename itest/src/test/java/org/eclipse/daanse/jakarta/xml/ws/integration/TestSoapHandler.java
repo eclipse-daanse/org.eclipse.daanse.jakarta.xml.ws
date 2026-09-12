16a51f3fb454ef7a82a575961681eb6a191a497f
Stefan Bischof <stbischof@bipolis.org>
2026-09-12 13:37:17 +0200

    Make the OSGi tests run again on JDK 25
    bnd 7.0.0's launcher sets a system-wide Policy, which JDK 24 removed, so the
    framework never started. The newer parent brings bnd 7.2.0; with it the tests
    also need the integration package the parent now looks for, and a runee that
    matches the Java release the parent compiles to.
    
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- itest/src/test/java/org/eclipse/daanse/jakarta/xml/ws/integration/TestSoapHandler.java
+++ itest/src/test/java/org/eclipse/daanse/jakarta/xml/ws/integration/TestSoapHandler.java
@@ -12,7 +12,7 @@
 *   Stefan Bischof (bipolis.org) - initial
 *   Christoph Läubrich - initial
 */
-package org.eclipse.daanse.jakarta.xml.ws.itests;
+package org.eclipse.daanse.jakarta.xml.ws.integration;
 
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
